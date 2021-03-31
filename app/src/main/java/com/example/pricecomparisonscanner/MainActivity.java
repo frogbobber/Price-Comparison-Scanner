041b17b3cd35110ff0a9eecc6966217901b82396
cdotson4 <38544442+cdotson4@users.noreply.github.com>
2021-03-30 23:08:41 -0600

    Added all of the basic data analysis functions

--- app/src/main/java/com/example/pricecomparisonscanner/MainActivity.java
+++ app/src/main/java/com/example/pricecomparisonscanner/MainActivity.java
@@ -5,16 +5,14 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+import com.example.pricecomparisonscanner.analysis.DataProcessor;
 import com.example.pricecomparisonscanner.information.AllProductInformation;
-import com.example.pricecomparisonscanner.information.ProductInformation;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
@@ -23,35 +21,25 @@ import org.json.JSONObject;
 
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ScrollView scrollView;
     private TextView textView;
-    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.resultsTextView);
+        textView = findViewById(R.id.textView);
 
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PackageManager.PERMISSION_GRANTED);
     }
 
     public void ScanButton(View view) {
         IntentIntegrator intentIntegrator = new IntentIntegrator(this);
-        intentIntegrator.initiateScan();
-
-    }
-
-    public void ViewAnalytics(View view) {
-        IntentIntegrator intentIntegrator = new IntentIntegrator(this);
-        intentIntegrator.initiateScan();
-
+        intentIntegrator.initiateScan(); //mayonaise
     }
 
     @Override
@@ -71,12 +59,12 @@ public class MainActivity extends AppCompatActivity {
                         try {
                             String inline = "";
                             System.out.println("lololol");
-                            //if run out of scams comment out here
                             URL url = new URL("https://api.upcitemdb.com/prod/trial/lookup?upc=" + intentResult.getContents());
                             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                             conn.setRequestMethod("GET"); // testtest
                             textView.setText("Connecting");
                             conn.connect();
+
 
                             int responseCode = conn.getResponseCode();
                             if (responseCode != 200) {
@@ -94,76 +82,23 @@ public class MainActivity extends AppCompatActivity {
                                 scanner.close();
                             }
 
-                            textView.setText("retrived from upcitemdb");
 
                             textView.setText("Got Response: ");
 
                             JSONObject jsonObject = new JSONObject(inline);
                             JSONArray jsonArray = jsonObject.getJSONArray("items").getJSONObject(0).getJSONArray("offers");
                             Double price1 = jsonArray.getJSONObject(0).getDouble("price");
+                            DataProcessor dp = new DataProcessor(jsonArray);
+                            System.out.println(dp.getBestListing());
 
-
-                            //textView.setText(price1 + "");
+                            //textView.setText("\n\n" + jsonArray.toString() + "");
                             String name = jsonArray.getJSONObject(0).getString("title").replaceAll(" ", "+");
-//                            AllProductInformation allProductInformation = WebScraper.getProductInformation(name);
-//                            textView.setText("\n\n" + name  + "\n\n" + allProductInformation + "");
-//                            System.out.println(allProductInformation + "");
-                            //to here
-                            //String name = "MONOPOLY+Game";//add this ->
                             AllProductInformation info = WebScraper.getProductInformation(name);
-                            info.setUpciteProducts(jsonObject);//and this one too
-                            info = new AllProductInformation(4, info);
-                            
-                            // textView.setText("\n\n" + name  + "\n\n" + info + ""); //Old Method
-                            
-                            StringBuilder outputBuilder = new StringBuilder();
-                            
-                            outputBuilder.append("Broad Sweep Database: \n");
-                            ArrayList<ProductInformation> products = info.getUpciteProducts();
-                            for (int i = 0; i < products.size(); i++) {
-                                outputBuilder.append(
-                                        products.get(i).getName().substring(0, Math.min(products.get(i).getName().length(), 20)) + "\n" +
-                                                products.get(i).getPrice().substring(0, Math.min(products.get(i).getPrice().length(), 20)) + "\n" +
-                                                products.get(i).getUrl().substring(0, Math.min(products.get(i).getUrl().length(), 20)) + "\n\n"
-                                );
-                            }
-
-                            outputBuilder.append("Top Walmart Listings: \n");
-                            products = info.getWalmartProducts();
-                            for (int i = 0; i < products.size(); i++) {
-                                outputBuilder.append(
-                                        products.get(i).getName().substring(0, Math.min(products.get(i).getName().length(), 20)) + "\n" +
-                                                products.get(i).getPrice().substring(0, Math.min(products.get(i).getPrice().length(), 20)) + "\n" +
-                                                products.get(i).getUrl().substring(0, Math.min(products.get(i).getUrl().length(), 20)) + "\n\n"
-                                );
-                            }
-
-                            outputBuilder.append("Top Amazon Listings: \n");
-                            products = info.getAmazonProducts();
-                            for (int i = 0; i < products.size(); i++) {
-                                outputBuilder.append(
-                                        products.get(i).getName().substring(0, Math.min(products.get(i).getName().length(), 20)) + "\n" +
-                                                products.get(i).getPrice().substring(0, Math.min(products.get(i).getPrice().length(), 20)) + "\n" +
-                                                products.get(i).getUrl().substring(0, Math.min(products.get(i).getUrl().length(), 20)) + "\n\n"
-                                );
-                            }
-
-                            outputBuilder.append("Top Best Buy Listings: \n");
-                            products = info.getTargetProducts();
-                            for (int i = 0; i < products.size(); i++) {
-                                outputBuilder.append(
-                                        products.get(i).getName().substring(0, Math.min(products.get(i).getName().length(), 20)) + "\n" +
-                                                products.get(i).getPrice().substring(0, Math.min(products.get(i).getPrice().length(), 20)) + "\n" +
-                                                products.get(i).getUrl().substring(0, Math.min(products.get(i).getUrl().length(), 20)) + "\n\n"
-                                );
-                            }
-                            
-                            textView.setText(outputBuilder.toString());
-                            
-                            System.out.println("\n\nnew info - \n" + info + "\n - end info\n\n");
+                            info.setUpciteProducts(jsonObject);
+                            textView.setText("\n\n" + name  + "\n\n" + info + "");
+                            //System.out.println(WebScraper.getProductInformation(name) + "");
 
                         } catch (Exception e) {
-                            e.printStackTrace();
                         }
                     }
                 });
---
+++ app/src/main/java/com/example/pricecomparisonscanner/analysis/Plotter.java
@@ -0,0 +1,5 @@
+package com.example.pricecomparisonscanner.analysis;
+
+public class Plotter {
+
+}
