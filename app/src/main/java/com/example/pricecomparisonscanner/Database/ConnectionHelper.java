0c2322b7bf56fee9b5b1d2e010e6e1bb67bb201e
frogbobber <78946249+frogbobber@users.noreply.github.com>
2021-04-24 12:27:08 -0600

    Added some stuff at the bottom of MainActivity to interact with mongo. This needs to be redone and put in a better place, but its a fine proof of concept.

--- app/src/main/java/com/example/pricecomparisonscanner/Database/ConnectionHelper.java
+++ app/src/main/java/com/example/pricecomparisonscanner/Database/ConnectionHelper.java
@@ -1,13 +1,40 @@
 package com.example.pricecomparisonscanner.Database;
-
-import java.sql.Connection;
-
+//
+//import android.os.StrictMode;
+//import android.util.Log;
+//
+//import java.sql.Connection;
+//import java.sql.Driver;
+//import java.sql.DriverManager;
+//
 public class ConnectionHelper {
-
-    private Connection connection;
-    private String uname, pw, ip, port, db;
-
+//
+//    private Connection connection;
+//    private String uname, pw, ip, port, db;
+//
 //    public Connection connectionClass() {
-//        ip = "";
+//        ip = Login.getIp();
+//        port = Login.getPort();
+//        pw = Login.getPassword();
+//        db = Login.getDatabase();
+//        uname = Login.getUsername();
+//
+//        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+//        StrictMode.setThreadPolicy(policy);
+//
+//        Connection connection = null;
+//
+//        String ConnectionURL = null;
+//
+//        try {
+//            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+//            ConnectionURL= "jdbc:jtds:sqlserver://" + ip + ":" + port + "/ " + db + ";" + "user=" + uname + ";password=" + pw + ";" + "databaseName=" + db;
+//            //connection = DriverManager.getConnection(ConnectionURL);
+//            connection = DriverManager.getConnection("jdbc:jtds:sqlserver://" + ip + ":" + port + "/" + db + ";databaseName=" + db, uname, pw);
+//        } catch (Exception e) {
+//            Log.e("Error ", e.getMessage());
+//        }
+//
+//        return connection;
 //    }
 }
